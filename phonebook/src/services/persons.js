63d3c8e4077ef3da2b0ac3c41f9201a355e55ef0
Pradhum Bansal <bansal.pradhumfeb@gmail.com>
2021-06-16 04:03:39 +0530

    3.10

--- phonebook/src/services/persons.js
+++ phonebook/src/services/persons.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
- const baseUrl = "http://localhost:3001/persons";
+ const baseUrl = "https://ancient-forest-73656.herokuapp.com/api/persons";
 
  const getAll = () =>
  {
-     const request = axios.get(baseUrl)
-      return request.then(response=> response.data)
+  return axios.get(baseUrl)
 
 }
 
